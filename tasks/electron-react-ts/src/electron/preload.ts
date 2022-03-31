d76a3f8e5b03269e3bf167283d0782696bb0808b
Jonathan Reyes <j@jreyes.org>
2022-03-31 18:16:40 -0500

    Add tasks sample with Electron, React, TS

---
+++ tasks/electron-react-ts/src/electron/preload.ts
@@ -0,0 +1,7 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  // Add preload code here.
+})
+
+export {}
