9519daf929ff5fb221d40152878658e2471d29e8
Ben Chatelain <ben@ditto.live>
2022-05-13 13:55:41 -0600

    🐛 Change applicationId to "live.dittolive.todo"

--- tasks/android-kotlin/ToDo/app/src/androidTest/java/com/dittolive/todo/ExampleInstrumentedTest.kt
+++ tasks/android-kotlin/ToDo/app/src/androidTest/java/com/dittolive/todo/ExampleInstrumentedTest.kt
@@ -16,6 +16,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dittolive.todo", appContext.packageName)
+        assertEquals("live.dittolive.todo", appContext.packageName)
     }
 }
