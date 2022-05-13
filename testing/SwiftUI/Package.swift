dc5230d5034c151ff8bcdab2fb617fef10b3131a
rae <633012+okdistribute@users.noreply.github.com>
2022-05-13 18:50:48 -0500

    Add testing example for SwiftUI (#19)
    * Add testing for SwiftUI
    
    * update name of app
    
    * Update swift test
    
    * make tests equal

---
+++ testing/SwiftUI/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUI",
+    platforms: [ .iOS(.v11), .macOS(.v11) ],
+    products: [
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/getditto/DittoSwiftPackage", branch: "main"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .testTarget(
+            name: "SwiftUITests",
+            dependencies: [
+                .product(name: "DittoObjC", package: "DittoSwiftPackage"),
+                .product(name: "DittoSwift", package: "DittoSwiftPackage")
+            ]
+        )
+    ]
+)
---
+++ testing/SwiftUI/README.md
@@ -0,0 +1,3 @@
+# SwiftUI
+
+A description of this package.
