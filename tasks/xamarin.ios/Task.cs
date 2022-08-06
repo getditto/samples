0ed6de7ad4c8e8da47187961de3582297310fe82
Walker Erekson <60948031+texasRanger09@users.noreply.github.com>
2022-08-05 18:01:28 -0700

    Update Task.cs

--- tasks/xamarin.ios/Task.cs
+++ tasks/xamarin.ios/Task.cs
@@ -1,4 +1,3 @@
-﻿// 1.
 using System;
 using System.Collections.Generic;
 using DittoSDK;
@@ -11,49 +10,11 @@ namespace Tasks
         public string body;
         public bool isCompleted;
 
-        public Task()
+        public Task(DittoDocument document)
         {
+            this._id = document["_id"].StringValue;
+            this.body = document["body"].StringValue;
+            this.isCompleted = document["isCompleted"].BooleanValue;
         }
-
-        // 3
-        public Task(string _id, string body, bool isCompleted)
-        {
-            this._id = _id;
-            this.body = body;
-            this.isCompleted = isCompleted;
-        }
-
-        //public Task(string body, bool isCompleted)
-        //{
-        //    this._id = Guid.NewGuid().ToString();
-        //    this.body = body;
-        //    this.isCompleted = isCompleted;
-        //}
-
-        //public Task(DittoDocument document)
-        //{
-        //    this._id = document["_id"].StringValue;
-        //    this.body = document["body"].StringValue;
-        //    this.isCompleted = document["isCompleted"].BooleanValue;
-        //}
-
-        //// 4.
-        //public override string ToString()
-        //{
-        //    return $"Task _id: {_id}, body: {body}, isCompleted: {isCompleted}";
-        //}
-
-        //// 5.
-        ///
-        public Dictionary<string, object> ToDictionary()
-        {
-            return new Dictionary<string, object>
-            {
-                { "_id", _id },
-                { "body", body },
-                { "isCompleted", isCompleted },
-            };
-        }
-
     }
 }
