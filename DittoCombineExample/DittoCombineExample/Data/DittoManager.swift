f711c3e0b61f31c012e86000b0db66c150abc4a2
Eric Turner <eric.turner@ditto.live>
2023-01-03 14:56:46 -0800

    DittoCombineExample: oops... remove offline key used for development

--- DittoCombineExample/DittoCombineExample/Data/DittoManager.swift
+++ DittoCombineExample/DittoCombineExample/Data/DittoManager.swift
@@ -8,10 +8,7 @@ final class DittoManager {
 
     private init() {
         ditto = Ditto()
-//        try! ditto.setOfflineOnlyLicenseToken("YOUR_OFFLINE_PORTAL_TOKEN")
-        try! ditto.setOfflineOnlyLicenseToken(
-            "o2d1c2VyX2lkdTExODE0NDU2ODIwNjc3NjI1MjAxN2ZleHBpcnl4GDIwMjMtMDEtMTVUMjA6MjM6MDAuMzg0WmlzaWduYXR1cmV4WGt5REdkbkhid3RqWjl1Nk5uTllhM1g1K1ZMc2ZWU1RTOUR5Q1p4TlVYUXhWRzM1RENWWHJBWWd0N3NUTEZVbmtmWEJsMXhoL1JOeWRZZXVZeEV0L1NnPT0="
-        )
+        try! ditto.setOfflineOnlyLicenseToken("YOUR_OFFLINE_PORTAL_TOKEN")
     }
     
     func startSync() {
