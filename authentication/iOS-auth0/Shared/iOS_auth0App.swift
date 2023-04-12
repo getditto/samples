90769a7f061df4e562a8688b70aeddd87c310da2
Eric Turner <eric.turner@ditto.live>
2023-04-12 09:19:05 -0700

    iOS authentication sample Ditto SDKv4.0.1, Auth0 SDK v2.3.2 (#95)
    This sample has been refactored to use new Auth0 async methods.
    Additionally, it was discovered that logging in and out repeatedly
    caused a crash due to a known issue with Ditto, where a Ditto instance
    resources are not released right away. A workaround was added to
    created a new persistence directory for each new login, and a cleanup
    function to clean up the file system when the app terminates.

--- authentication/iOS-auth0/Shared/iOS_auth0App.swift
+++ authentication/iOS-auth0/Shared/iOS_auth0App.swift
@@ -1,6 +1,6 @@
 //
 //  iOS_auth0App.swift
-//  Shared
+//  iOS-auth0
 //
 //  Created by Karissa McKelvey on 3/9/22.
 //
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct iOS_auth0App: App {
+    /// This helper is not required for demonstrating Ditto authentication.
+    /// See Helpers.swift
+    private var cleanup = CleanupHelper()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
