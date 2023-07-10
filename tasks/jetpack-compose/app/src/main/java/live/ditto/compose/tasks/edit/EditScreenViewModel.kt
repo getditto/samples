8d83e56f50e2f5bc8fbc671c8d02026f58d54e4e
Fuad Kamal <fuad@anaara.com>
2023-07-10 15:34:57 -0400

    👷🏾remove force unwraps
    null safety

--- tasks/jetpack-compose/app/src/main/java/live/ditto/compose/tasks/edit/EditScreenViewModel.kt
+++ tasks/jetpack-compose/app/src/main/java/live/ditto/compose/tasks/edit/EditScreenViewModel.kt
@@ -18,9 +18,9 @@ class EditScreenViewModel: ViewModel() {
     fun setupWithTask(taskId: String?) {
         canDelete.postValue(taskId != null)
         val taskId: String = taskId?.let { it } ?: return;
-        val doc: DittoDocument = TasksApplication.ditto!!.store["tasks"]
-            .findById(DittoDocumentId(taskId))
-            .exec()?.let { it } ?: return;
+        val doc: DittoDocument = TasksApplication.ditto?.store?.get("tasks")
+            ?.findById(DittoDocumentId(taskId))
+            ?.exec()?.let { it } ?: return;
         val task = Task(doc)
         _id = task._id
         body.postValue(task.body)
@@ -31,26 +31,29 @@ class EditScreenViewModel: ViewModel() {
     fun save() {
         if (_id == null) {
             // save
-            TasksApplication.ditto!!.store["tasks"]
-                .upsert(mapOf(
+            TasksApplication.ditto?.store?.get("tasks")
+                ?.upsert(mapOf(
                     "body" to body.value,
                     "isCompleted" to isCompleted.value
                 ))
         } else {
             // update
-            TasksApplication.ditto!!.store["tasks"].findById(DittoDocumentId(_id!!))
-                .update { mutableDoc ->
-                    val mutableDoc = mutableDoc?.let { it } ?: return@update
-                    mutableDoc["body"].set(body.value ?: "")
-                    mutableDoc["isCompleted"].set(isCompleted.value ?: "")
-                }
+            _id?.let { _id ->
+                TasksApplication.ditto?.store?.get("tasks")?.findById(DittoDocumentId(_id))
+                    ?.update { dittoDocument ->
+                        val mutableDoc = dittoDocument ?: return@update
+                        mutableDoc["body"].set(body.value ?: "")
+                        mutableDoc["isCompleted"].set(isCompleted.value ?: "")
+                    }
+            }
+
         }
     }
 
     // 4.
     fun delete() {
-        TasksApplication.ditto!!.store["tasks"].upsert(mapOf(
-            "_id" to _id!!,
+        TasksApplication.ditto?.store?.get("tasks")?.upsert(mapOf(
+            "_id" to _id,
             "isDeleted" to true
         ))
     }
--- tasks/jetpack-compose/app/src/main/java/live/ditto/compose/tasks/list/TasksListScreenViewModel.kt
+++ tasks/jetpack-compose/app/src/main/java/live/ditto/compose/tasks/list/TasksListScreenViewModel.kt
@@ -9,17 +9,17 @@ import live.ditto.compose.tasks.data.Task
 class TasksListScreenViewModel: ViewModel() {
     val tasks: MutableLiveData<List<Task>> = MutableLiveData(emptyList())
 
-    val subscription = TasksApplication.ditto!!.store["tasks"]
-        .find("!isDeleted").subscribe()
-    val liveQuery = TasksApplication.ditto!!.store["tasks"]
-        .find("!isDeleted").observeLocal { docs, _ ->
+    val subscription = TasksApplication.ditto?.store?.get("tasks")
+        ?.find("!isDeleted")?.subscribe()
+    val liveQuery = TasksApplication.ditto?.store?.get("tasks")
+        ?.find("!isDeleted")?.observeLocal { docs, _ ->
             tasks.postValue(docs.map { Task(it) })
         }
 
     fun toggle(taskId: String) {
-        TasksApplication.ditto!!.store["tasks"]
-            .findById(DittoDocumentId(taskId))
-            .update { mutableDoc ->
+        TasksApplication.ditto?.store?.get("tasks")
+            ?.findById(DittoDocumentId(taskId))
+            ?.update { mutableDoc ->
                 val mutableDoc = mutableDoc?.let { it } ?: return@update
                 mutableDoc["isCompleted"].set(!mutableDoc["isCompleted"].booleanValue)
             }
@@ -27,7 +27,7 @@ class TasksListScreenViewModel: ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
-        liveQuery.close()
-        subscription.close()
+        liveQuery?.close()
+        subscription?.close()
     }
 }
