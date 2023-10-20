7d62e17194f8a120f90c7631ced01b0d3b09ecb8
okdistribute <633012+okdistribute@users.noreply.github.com>
2023-10-20 10:14:36 -0700

    Remove unused tasks folder and move up CPP code interim

--- tasks/android-kotlin/ToDo/app/src/main/java/com/dittolive/todo/TaskViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.dittolive.todo
-
-import androidx.recyclerview.widget.RecyclerView
-import live.dittolive.todo.databinding.TaskViewBinding
-
-class TaskViewHolder(val binding: TaskViewBinding): RecyclerView.ViewHolder(binding.root)
